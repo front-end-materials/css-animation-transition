fa1e024801d0e14f6746833ecbb2d836cf9a7544
Dave Everitt <dave.everitt@icloud.com>
2022-05-29 15:11:49 +0100

    try touch instead of mouseenter

--- compact-slideout-menu/scripts.js
+++ compact-slideout-menu/scripts.js
@@ -7,7 +7,7 @@ const slidein = () => {
   menu.style="transform: translateX(-8em)";
 }
 
-menu.addEventListener("mouseover", slideout)
-menu.addEventListener("mouseleave", slidein)
+menu.addEventListener("ontouchstart", slideout)
+menu.addEventListener("ontouchend", slidein)
 
 // ontouchstart ontouchend
