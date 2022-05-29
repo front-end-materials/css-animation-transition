82c6545a12ef47cb6a88b89a048320b3f3b7c19b
Dave Everitt <dave.everitt@icloud.com>
2022-05-29 16:08:18 +0100

    replace hover with js

--- compact-slideout-menu/scripts.js
+++ compact-slideout-menu/scripts.js
@@ -1,15 +1,7 @@
 const menu = document.getElementById("menu");
 
 const slideout = () => {
-  if (menu.style="transform: translateX(0)") {
-    menu.style="transform: translateX(-8em)";
-  };
+  menu.classList.toggle("menushow");
 }
-// const slidein = () => {
-//   menu.style="transform: translateX(-8em)";
-// }
 
-menu.addEventListener("onclick", slideout)
-// menu.addEventListener("ontouchend", slidein)
-
-// ontouchstart ontouchend
+menu.addEventListener("click", slideout);
